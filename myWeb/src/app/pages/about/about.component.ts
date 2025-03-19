25dafc0c318b82cf1d3f92bbdd69a838250f696e
ngoctuyen1 <tuyennn22411@st.uel.edu.vn>
2025-03-19 10:21:12 +0700

    about page

--- myWeb/src/app/pages/about/about.component.ts
+++ myWeb/src/app/pages/about/about.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, RouterLink, RouterLinkActive], 
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment: string | null) => {
+      if (fragment) {
+        this.scrollToSection(fragment);
+      }
+    });
+  }
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
